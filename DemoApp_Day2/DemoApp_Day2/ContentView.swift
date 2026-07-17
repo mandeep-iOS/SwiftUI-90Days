132b5222e71b2ffe033980e4c96b8cb7f9d086a7
Mandeep Singh <mdeep14293@gmail.com>
2026-07-17 00:22:37 -0400

    Day 2 - Form, List, Detail demo

---
+++ DemoApp_Day2/DemoApp_Day2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoApp_Day2
+//
+//  Created by Deep Baath on 2026-07-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoApp_Day2/DemoApp_Day2/DemoApp_Day2App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp_Day2App.swift
+//  DemoApp_Day2
+//
+//  Created by Deep Baath on 2026-07-17.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp_Day2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
